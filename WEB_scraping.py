fac40373a52077b632a500114b4b380b70f417e8
Farida Ousmailou <faridaousmailou51@gmail.com>
2024-02-09 22:51:53 +0100

    add print

--- WEB_scraping.py
+++ WEB_scraping.py
@@ -3,26 +3,27 @@ from bs4 import BeautifulSoup
 import csv
 
 url = "https://www.gov.uk/search/news-and-communications"
+
 page = requests.get(url)
-#html_code = page.text
 page_content = page.content
 soup = BeautifulSoup(page_content,"html.parser")
 print(soup.title.string)
 class_name = "govuk-link"
+
 titres = soup.find_all("a", class_=class_name)
-#print(titres)
 descriptions = soup.find("p", class_="gem-c-document-list__item-description")
-#print(descriptions)
+print(descriptions.string)
+
 titres_textes = []
 for titre in titres:
     titres_textes.append(titre.string)
-#print(titres_textes)
+print(titres_textes)
 
 
 descriptions_textes = []
 for description in descriptions:
     descriptions_textes.append(description.string)
-#print(descriptions_textes)
+print(descriptions_textes)
 
 
 en_tete = ["titre", "description"]
