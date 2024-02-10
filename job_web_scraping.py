1e8ee5af274cc2b9f25470def269b27091bc1426
Farida Ousmailou <faridaousmailou51@gmail.com>
2024-02-10 18:46:56 +0100

    add csv_file

--- job_web_scraping.py
+++ job_web_scraping.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
 
 # type GET request
 r = requests.get("https://www.jobijoba.com/fr/annonce/54/26e1c6007076b02ca4ca43b4d7d3f51b")
@@ -11,14 +12,26 @@ print(page_html)
 soup = BeautifulSoup(page_html, "lxml")
 
 # getting title
-job_title= soup.title.text
+job_title = soup.title.text
 print(job_title)
 
 # getting mainly tag
 tags = soup.find("div", {"class": "row permalink-infos d-flex align-items-center px-3"})
-print(tags.text)
+job_tags = tags.text
+print(job_tags)
 
 # job offer text
 description = soup.find("p", {"class": "permalink-description"})
-print(description.text)
+job_description= description.text
+print(job_description)
+
 #print((soup.find("div", {"class": "permalink-subtitle" })).text)
+
+column_names = ["job_title", "tags", "description"]
+job_data = [job_title, job_tags, job_description]
+
+with open("job_offers.csv", "w", encoding="utf-8", newline="") as csv_file:
+    writer = csv.writer(csv_file, delimiter=",")
+    if csv_file.tell == 0:
+        writer.writerow(column_names)
+    writer.writerow(job_data)
