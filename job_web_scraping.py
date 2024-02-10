790de8085c76203a18d57eb04a6eb224683d1f08
Farida Ousmailou <faridaousmailou51@gmail.com>
2024-02-10 01:15:07 +0100

    Scrap job details from a specific URL and print them.

--- job_web_scraping.py
+++ job_web_scraping.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 # type GET request
-r = requests.get("https://www.jobijoba.com/fr/annonce/54/18adbb3d68f9217048763ca416deb342")
+r = requests.get("https://www.jobijoba.com/fr/annonce/54/26e1c6007076b02ca4ca43b4d7d3f51b")
 # content of r
 page_html = r.text
 print(page_html)
@@ -11,7 +11,14 @@ print(page_html)
 soup = BeautifulSoup(page_html, "lxml")
 
 # getting title
-print(soup.title.text)
+job_title= soup.title.text
+print(job_title)
 
 # getting mainly tag
-print((soup.find("div", {"class": "row permalink-infos d-flex align-items-center px-3"})).text)
+tags = soup.find("div", {"class": "row permalink-infos d-flex align-items-center px-3"})
+print(tags.text)
+
+# job offer text
+description = soup.find("p", {"class": "permalink-description"})
+print(description.text)
+#print((soup.find("div", {"class": "permalink-subtitle" })).text)
